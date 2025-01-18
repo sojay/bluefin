c212a3c20e6ccc389702e89c7b856646727ac281
Sammie <sojaysings@gmail.com>
2025-01-18 02:27:30 -0500

    fix: clean build

--- build.sh
+++ build.sh
@@ -3,31 +3,31 @@ set -ouex pipefail
 
 RELEASE="$(rpm -E %fedora)"
 
-dnf5 -y copr enable ublue-os/staging
-dnf5 -y copr enable ublue-os/akmods
+# dnf5 -y copr enable ublue-os/staging
+# dnf5 -y copr enable ublue-os/akmods
 
-skopeo copy docker://ghcr.io/ublue-os/akmods-extra:surface-"${RELEASE}" dir:/tmp/akmods-extra-rpms
-AKMODS_EXTRA_TARGZ=$(jq -r '.layers[].digest' < /tmp/akmods-extra-rpms/manifest.json | cut -d : -f 2)
-tar -xvzf /tmp/akmods-extra-rpms/"$AKMODS_EXTRA_TARGZ" -C /tmp/
-mv /tmp/rpms/* /tmp/akmods-extra-rpms/
+# skopeo copy docker://ghcr.io/ublue-os/akmods-extra:surface-"${RELEASE}" dir:/tmp/akmods-extra-rpms
+# AKMODS_EXTRA_TARGZ=$(jq -r '.layers[].digest' < /tmp/akmods-extra-rpms/manifest.json | cut -d : -f 2)
+# tar -xvzf /tmp/akmods-extra-rpms/"$AKMODS_EXTRA_TARGZ" -C /tmp/
+# mv /tmp/rpms/* /tmp/akmods-extra-rpms/
 
 
-if [[ $SUFFIX == *"nvidia"* ]]; then
-  skopeo copy docker://ghcr.io/ublue-os/akmods-nvidia-open:surface-"${RELEASE}" dir:/tmp/akmods-nvidia-open-rpms
-  AKMODS_NVIDIA_OPEN_TARGZ=$(jq -r '.layers[].digest' < /tmp/akmods-nvidia-open-rpms/manifest.json | cut -d : -f 2)
-  tar -xvzf /tmp/akmods-nvidia-open-rpms/"$AKMODS_NVIDIA_OPEN_TARGZ" -C /tmp/
-  mv /tmp/rpms/* /tmp/akmods-nvidia-open-rpms/
+# if [[ $SUFFIX == *"nvidia"* ]]; then
+#   skopeo copy docker://ghcr.io/ublue-os/akmods-nvidia-open:surface-"${RELEASE}" dir:/tmp/akmods-nvidia-open-rpms
+#   AKMODS_NVIDIA_OPEN_TARGZ=$(jq -r '.layers[].digest' < /tmp/akmods-nvidia-open-rpms/manifest.json | cut -d : -f 2)
+#   tar -xvzf /tmp/akmods-nvidia-open-rpms/"$AKMODS_NVIDIA_OPEN_TARGZ" -C /tmp/
+#   mv /tmp/rpms/* /tmp/akmods-nvidia-open-rpms/
 
-  rpm --erase kmod-nvidia --nodeps ; 
-fi
+#   rpm --erase kmod-nvidia --nodeps ; 
+# fi
 
 # Install Surface-specific packages
 dnf5 -y install --allowerasing \
   iptsd \
   libwacom-surface
 
-dnf5 -y copr enable ublue-os/staging
-dnf5 -y copr enable ublue-os/akmods
+# dnf5 -y copr enable ublue-os/staging
+# dnf5 -y copr enable ublue-os/akmods
 
 # Install additional packages
 rpm-ostree install screen
@@ -36,5 +36,5 @@ rpm-ostree install screen
 # systemctl enable podman.socket
 
 # Cleanup
-dnf5 -y copr disable ublue-os/staging
-dnf5 -y copr disable ublue-os/akmods
+# dnf5 -y copr disable ublue-os/staging
+# dnf5 -y copr disable ublue-os/akmods
